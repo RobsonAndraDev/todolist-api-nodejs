1486aaf4968ba7fa9beb13529078c8db8f6707ba
RobsonAndradev <robsom.s.a@gmail.com>
2019-10-26 22:35:06 -0300

    Fix cors issue on post

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.use( bodyParser.urlencoded({ extended: false }) );
 app.use( bodyParser.json() );
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
 todo.set( app );
